7452fa287a8459197be260a51744dea65783bd1e
Ewerton silva <ewertoncom297@gmail.com>
2020-11-17 19:49:41 -0300

    implementando tela splash

---
+++ Appiscina/app/src/main/java/com/example/appiscina/telaSplash.java
@@ -0,0 +1,42 @@
+package com.example.appiscina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Trace;
+import android.view.WindowManager;
+
+public class telaSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_splash);
+        getSupportActionBar().hide();
+
+        Thread timerThread = new Thread() {
+
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(telaSplash.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void  onPause(){
+        super.onPause();
+        finish();
+    }
+}
+
