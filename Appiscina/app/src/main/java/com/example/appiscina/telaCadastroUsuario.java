784aaac3f8a9c7fb6a78949389253461892d6635
Ewerton silva <ewertoncom297@gmail.com>
2020-11-30 22:27:15 -0300

    Adicionando tela cadastro de usuário

---
+++ Appiscina/app/src/main/java/com/example/appiscina/telaCadastroUsuario.java
@@ -0,0 +1,37 @@
+package com.example.appiscina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class telaCadastroUsuario extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro_usuario);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
+        getSupportActionBar().setTitle("Cadastro");
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, telaLogin.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+}
--- Appiscina/app/src/main/java/com/example/appiscina/telaLogin.java
+++ Appiscina/app/src/main/java/com/example/appiscina/telaLogin.java
@@ -2,14 +2,31 @@ package com.example.appiscina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class telaLogin extends AppCompatActivity {
+
+    Button bt_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
         getSupportActionBar().hide();
+
+
+        bt_cadastro = findViewById(R.id.bt_cadastro);
+
+        bt_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),telaCadastroUsuario.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
