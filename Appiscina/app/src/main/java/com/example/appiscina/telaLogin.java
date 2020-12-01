7e725fc277633d2f2040da495b45a8df90ce8983
Ewerton silva <ewertoncom297@gmail.com>
2020-11-30 23:04:20 -0300

    Adicionando tela de redefinição de senha

--- Appiscina/app/src/main/java/com/example/appiscina/telaLogin.java
+++ Appiscina/app/src/main/java/com/example/appiscina/telaLogin.java
@@ -6,11 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class telaLogin extends AppCompatActivity {
 
     Button bt_cadastro;
-
+    TextView tvRecuperacaoSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +20,7 @@ public class telaLogin extends AppCompatActivity {
 
 
         bt_cadastro = findViewById(R.id.bt_cadastro);
+        tvRecuperacaoSenha = findViewById(R.id.tvRecuperacaoSenha);
 
         bt_cadastro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +29,15 @@ public class telaLogin extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        tvRecuperacaoSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),telaRedefinicaoSenha.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
