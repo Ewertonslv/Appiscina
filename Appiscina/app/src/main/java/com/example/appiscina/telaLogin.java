d940b10222c168f50656a9bfc5924a76dfa5f39e
Ewerton silva <ewertoncom297@gmail.com>
2020-12-04 09:31:24 -0300

    Implementando verificações de usuário tela login

--- Appiscina/app/src/main/java/com/example/appiscina/telaLogin.java
+++ Appiscina/app/src/main/java/com/example/appiscina/telaLogin.java
@@ -6,21 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class telaLogin extends AppCompatActivity {
 
-    Button bt_cadastro;
+    Button bt_cadastro, bt_login;
     TextView tvRecuperacaoSenha;
+
+    private EditText username;
+    private EditText senha;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
         getSupportActionBar().hide();
 
-
+        bt_login = findViewById(R.id.bt_login);
         bt_cadastro = findViewById(R.id.bt_cadastro);
         tvRecuperacaoSenha = findViewById(R.id.tvRecuperacaoSenha);
+        username =  findViewById(R.id.username);
+        senha =  findViewById(R.id.senha);
 
         bt_cadastro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +38,38 @@ public class telaLogin extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usuario = username.getText().toString();
+                String password = senha.getText().toString();
+
+                if(usuario.isEmpty()){
+                    username.setError("Campo obrigatório");
+                    return;
+                }
+
+                if(password.isEmpty()){
+                    senha.setError("Campo obrigatório");
+                    return;
+                }
+
+                if(usuario.equals("ewerton") && (password.equals("123"))) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+
+                    finish();
+
+                }else{
+                    //Toast.makeText(this, "usuário ou senha incorreto!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
 
         tvRecuperacaoSenha.setOnClickListener(new View.OnClickListener() {
             @Override
