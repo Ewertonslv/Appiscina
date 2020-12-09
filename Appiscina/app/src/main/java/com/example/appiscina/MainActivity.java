5d56a3fb4d708877b651fbcf5649ad184dd23f53
Ewerton silva <ewertoncom297@gmail.com>
2020-12-08 22:32:58 -0300

    Implementações iniciais na tela principal

--- Appiscina/app/src/main/java/com/example/appiscina/MainActivity.java
+++ Appiscina/app/src/main/java/com/example/appiscina/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.appiscina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Imóveis");
+
+        //imageView.setVisibility(View.GONE);
+
     }
+
 }
