671dd20a200f201e2d5c5d3068a8a3ce4a15bc3f
Ewerton silva <ewertoncom297@gmail.com>
2020-12-12 10:42:21 -0300

    Iniciando layout da tela de cadastro de propriedade

--- Appiscina/app/src/main/java/com/example/appiscina/tela2CadastroPropriedade.java
+++ Appiscina/app/src/main/java/com/example/appiscina/tela2CadastroPropriedade.java
@@ -5,33 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 
-public class telaCadastroPropriedade extends AppCompatActivity {
-
-    ImageView setaProximo;
-
+public class tela2CadastroPropriedade extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_cadastro_propriedade);
+        setContentView(R.layout.activity_tela2_cadastro_propriedade);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
         getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
         getSupportActionBar().setTitle("Cadastrar propriedade");
-
-        setaProximo = findViewById(R.id.setaProximo);
-
-        setaProximo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),tela2CadastroPropriedade.class);
-                startActivity(i);
-            }
-        });
-
     }
 
     @Override
@@ -39,7 +23,7 @@ public class telaCadastroPropriedade extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
-                startActivity(new Intent(this, addPropriedade.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                startActivity(new Intent(this, telaCadastroPropriedade.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
                 finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
                 break;
             default:break;
